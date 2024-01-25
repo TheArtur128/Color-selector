5fbff77db829621a71bc8f39bf7088625f678036
TheArtur128 <88573504+TheArtur128@users.noreply.github.com>
2024-01-25 23:31:44 +0700

    chore(`vectors`): add functor to `number`

--- scripts/src/vectors.ts
+++ scripts/src/vectors.ts
@@ -4,14 +4,25 @@ export type Vector3 = {
     z: number
 }
 
-export function map(
-    operation: (a: number, b: number) => number,
+export function combine(
     first: Vector3,
     second: Vector3,
+    operation: (a: number, b: number) => number,
 ): Vector3 {
     return {
         x: operation(first.x, second.x),
         y: operation(first.y, second.y),
         z: operation(first.z, second.z),
+    }
+}
+
+export function map(
+    vector: Vector3,
+    operation: (v: number) => number,
+): Vector3 {
+    return {
+        x: operation(vector.x),
+        y: operation(vector.y),
+        z: operation(vector.z),
     }
 }
